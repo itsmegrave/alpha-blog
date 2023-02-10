ed4860ec65dd5f6515e8b411c737f3309f8591b2
Samuel Grave <samuel.grave.silva@outlook.com>
2023-02-10 12:27:27 -0300

    Add delete user and dependent articles destroy functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
+
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -36,6 +37,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Account and all associated articles successfully deleted'
+    redirect_to articles_path
+  end
+
   private
 
   def user_params
@@ -45,9 +53,11 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-    def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own account"
-      redirect_to @user
-    end
+
+  def require_same_user
+    return unless current_user != @user
+
+    flash[:alert] = 'You can only edit your own account'
+    redirect_to @user
+  end
 end
