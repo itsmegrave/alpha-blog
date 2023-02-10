e164ceff54ad5d196381622797a79937d9a0392b
Samuel Grave <samuel.grave.silva@outlook.com>
2023-02-10 12:01:51 -0300

    Modify layout and redirect link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new'
     end
