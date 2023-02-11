cd4fb4105b7116e1b99365c38ab92cbc158cc517
Samuel Grave <samuel.grave.silva@outlook.com>
2023-02-11 13:28:44 -0300

    Add admin user requirement to categories new & create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: %i[index show]
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -26,5 +27,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    return if logged_in? && current_user.admin?
+
+    flash[:alert] = "Only admins can perform that action"
+    redirect_to categories_path
   end
 end
