6c80b40ea6d9714699a661162b9be997dc239367
Samuel Grave <samuel.grave.silva@outlook.com>
2023-02-11 10:29:09 -0300

    runs rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   before_action :require_user, only: %i[edit update]
   before_action :require_same_user, only: %i[edit update destroy]
 
-  def show
-    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
-
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -17,30 +17,31 @@ class UsersController < ApplicationController
 
   def edit; end
 
-  def update
-    if @user.update(user_params)
-      flash[:notice] = 'Your account information was successfully updated'
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      flash[:notice] =
+        "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
-      render 'new'
+      render "new"
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
+    else
+      render "edit"
     end
   end
 
   def destroy
     @user.destroy
     session[:user_id] = nil if @user == current_user
-    flash[:notice] = 'Account and all associated articles successfully deleted'
+    flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to articles_path
   end
 
@@ -57,7 +58,7 @@ class UsersController < ApplicationController
   def require_same_user
     return unless current_user != @user && !current_user.admin?
 
-    flash[:alert] = 'You can only edit or delete your own account'
+    flash[:alert] = "You can only edit or delete your own account"
     redirect_to @user
   end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,20 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  test 'category should be valid' do
-    @category = Category.new(name: 'Sports')
-
+    def setup
+        @category = Category.new(name: "Sports")
+    end
+  test "category should be valid" do
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = ' '
+
+    assert_not @category.valid?
+  end
+  test "name should be unique" do
   end
 end
